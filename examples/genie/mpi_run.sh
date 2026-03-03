01b2543b28fe3855106f8d21452a4e132e67ce04
Jit Gupta <gjit@ctolab-srv08.englab.juniper.net>
2026-03-03 10:53:23 -0800

    Branch for gjit to make genie runtime changes

--- examples/genie/mpi_run.sh
+++ examples/genie/mpi_run.sh
@@ -6,12 +6,12 @@ PROJECT_DIR="${SCRIPT_DIR:?}/../../"
 EXAMPLE_DIR="${PROJECT_DIR:?}/examples/genie"
 
 # paths
-WORKLOAD="${EXAMPLE_DIR}/workload/ALL_GATHER"
+WORKLOAD="${EXAMPLE_DIR}/generator/genie_micro_AR_4npu_1024MB_10r/microbench"
 SYSTEM="${EXAMPLE_DIR:?}/system_2chunk.json"
 REMOTE_MEMORY="${EXAMPLE_DIR:?}/remote_memory.json"
 LOGICAL_TOPOLOGY="${EXAMPLE_DIR:?}/logical_topology_4.json"
 NUM_RANKS=4
-RDMA_DRIVER="mlx5_0"
+RDMA_DRIVER="mlx5_3"
 RDMA_PORT=1
 
 JOBTAG=$(date +%m%d_%H%M%S);
@@ -22,8 +22,11 @@ JOBTAG=$(date +%m%d_%H%M%S);
 export CHROMETRACE_FILENAME_DATETIME=${JOBTAG}
 mpirun \
     --tag-output \
+    --mca oob_tcp_if_include enp181s0np0 --mca btl_tcp_if_include enp181s0np0 --allow-run-as-root \
     -np ${NUM_RANKS} \
     -N 1 \
+    -H ctolab-srv08:1,ctolab-srv07:1,ctolab-srv06:1,ctolab-srv05:1 \
+    -x IBVERBS_INTERCEPT_EXP_TAG="genie_ibv_trace_${JOBTAG}" -x LD_PRELOAD="/mnt/aiml/jinsun/ibverbs_intercept/libibverbs_intercept.so" \
     taskset --cpu-list 8 \
     ${PROJECT_DIR}/build/astra_genie/build/bin/AstraSim_Genie \
     --workload "${WORKLOAD}" \
@@ -31,5 +34,6 @@ mpirun \
     --memory "${REMOTE_MEMORY}"  \
     --logical_topology "${LOGICAL_TOPOLOGY}" \
     --rdma_driver "${RDMA_DRIVER}" \
-    --rdma_port "${RDMA_PORT}" > \
-    "${PROJECT_DIR}/output_${JOBTAG}.log" 2>&1
+    --rdma_port "${RDMA_PORT}"
+# > \
+#    "${PROJECT_DIR}/output_${JOBTAG}.log" 2>&1

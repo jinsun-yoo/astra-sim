52f9073146c425a28aa91f79d0766da28ce8f6d9
Jinsun Yoo <jinsunyoo332@gmail.com>
2026-03-29 03:17:56 +0200

    Misc updates to run_sweep

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -x
 
 TIMETAG=$(date +%m%d_%H%M%S)
 OUTPUT_PATH=${OUTPUT_PATH:-outputs/${TIMETAG}}
@@ -9,7 +9,6 @@ echo "Output will be saved to ${OUTPUT_PATH}"
 
 export LD_LIBRARY_PATH=/nfs/jinsun/ibverbs_intercept:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/nfs/jinsun/nccl/build/lib:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=/nfs/jinsun/nccl/ext-profiler/example:$LD_LIBRARY_PATH
 module load openmpi
 
 for size in 8 16 32 64 128 256 512 1024 2048; do
@@ -29,9 +28,8 @@ for size in 8 16 32 64 128 256 512 1024 2048; do
     NCCL_IB_ADAPTIVE_ROUTING=0 \
     LD_PRELOAD=/nfs/jinsun/ibverbs_intercept/libibverbs_intercept.so  \
     IBVERBS_INTERCEPT_EXP_TAG=nccl_${JOBTAG} \
-    NCCL_PROFILER_DUMP_FILE=nccl_${JOBTAG} \
     mpirun -np 4 -N 1 ${NCCL_TEST_PATH}/build/all_reduce_perf \
-    -b ${size}M -e ${size}M -n 10 -w 0 -c 0 > nccl_output_${JOBTAG}.log
+    -b ${size}M -e ${size}M -n 30 -w 0 -c 0 > nccl_output_${JOBTAG}.log
 done
 mv nccl_output_size* "${OUTPUT_PATH}/"
 mv nccl_size*.json "${OUTPUT_PATH}/"

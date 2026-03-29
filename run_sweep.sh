21259a8e0ea07b7efd3508f271da015dbbfdc52f
Jinsun Yoo <jinsunyoo332@gmail.com>
2026-03-29 03:18:09 +0200

    Fix HCA to one nic in nccl-test for run_sweep test

--- run_sweep.sh
+++ run_sweep.sh
@@ -26,6 +26,7 @@ mv genie_ibv_trace_size* "${OUTPUT_PATH}/"
 for size in 8 16 32 64 128 256 512 1024 2048; do
     JOBTAG=size_${size} 
     NCCL_IB_ADAPTIVE_ROUTING=0 \
+    NCCL_IB_HCA=mlx5_0 \
     LD_PRELOAD=/nfs/jinsun/ibverbs_intercept/libibverbs_intercept.so  \
     IBVERBS_INTERCEPT_EXP_TAG=nccl_${JOBTAG} \
     mpirun -np 4 -N 1 ${NCCL_TEST_PATH}/build/all_reduce_perf \

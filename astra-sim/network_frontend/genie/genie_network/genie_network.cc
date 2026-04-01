bab95ed4739144a9e5a79b9a7cd006ce6d07dae3
Jinsun Yoo <jinsunyoo332@gmail.com>
2026-04-02 01:22:14 +0200

    Account for receiving messages from next ring while waiting for send

--- astra-sim/network_frontend/genie/genie_network/genie_network.cc
+++ astra-sim/network_frontend/genie/genie_network/genie_network.cc
@@ -397,9 +397,27 @@ void ASTRASimGenieNetwork::sim_recv_handler(FuncArgs *fun_args) {
 void ASTRASimGenieNetwork::mark_complete(int qp_idx, AstraSim::sim_request& snd_req, AstraSim::sim_request& rcv_req, bool is_send) {
     if (simple_ring_ptr) {
         if (is_send) {
-            simple_ring_ptr->mark_send_complete(qp_idx, snd_req, rcv_req);
+            if (simple_ring_ptr->polled_send_cnt[qp_idx] >= simple_ring_ptr->num_msgs_per_qp) {
+                // This case unlikely, but add to match symmetry.
+                PollArgs *args = poll_send_args->get_slot_to_write();
+                args->qp_idx = qp_idx;
+                args->snd_req = snd_req;
+                args->rcv_req = rcv_req;
+                pending_poll_sends.push(args);
+            } else {
+                simple_ring_ptr->mark_send_complete(qp_idx, snd_req, rcv_req);
+            }
         } else {
-            simple_ring_ptr->mark_recv_complete(qp_idx, snd_req, rcv_req);
+            // Might receive requests for next ring when waiting for sends for this ring
+            if (simple_ring_ptr->polled_recv_cnt[qp_idx] >= simple_ring_ptr->num_msgs_per_qp) {
+                PollArgs *args = poll_recv_args->get_slot_to_write();
+                args->qp_idx = qp_idx;
+                args->snd_req = snd_req;
+                args->rcv_req = rcv_req;
+                pending_poll_recvs.push(args);
+            } else {
+                simple_ring_ptr->mark_recv_complete(qp_idx, snd_req, rcv_req);
+            }
         }
     } else {
         if (is_send) {

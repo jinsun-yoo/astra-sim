b751fe1fd8402d0e00025b0b495f74dba4e884ea
Jinsun Yoo <jinsunyoo332@gmail.com>
2026-04-05 13:48:48 -0400

    Do not assert id > 0 for commgroups

--- astra-sim/system/CommunicatorGroup.cc
+++ astra-sim/system/CommunicatorGroup.cc
@@ -40,9 +40,6 @@ CommunicatorGroup::~CommunicatorGroup() {
 }
 
 void CommunicatorGroup::set_id(int id) {
-    // id 0 is reserved for the default comm group, which is all ranks.
-    // CTRL+F "default communication group"
-    assert(id > 0);
     this->id = id;
     this->num_streams = id * 1000000;
 }

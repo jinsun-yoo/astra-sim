c3603add8a8ad78c58576158a0fdd1f3c69ee86f
Jinsun Yoo <jinsunyoo332@gmail.com>
2026-05-01 08:21:40 +0200

    Scripts-1
    git commit -m "Scripts-2"
    
    Expand nccl_path in run scripts

---
+++ run_sweep_genie.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+
+TIMETAG=$(date +%m%d_%H%M%S)
+COLLECTIVE=${COLLECTIVE:-all_reduce}
+COLLECTIVE_UPPER=${COLLECTIVE^^}
+ROOT_PATH=${ROOT_PATH:-/nfs/jinsun}
+RUN_SCRIPT=${RUN_SCRIPT:-mpi_run_micro.sh}
+mkdir -p "${OUTPUT_PATH}"
+echo "Output will be saved to ${OUTPUT_PATH}"
+
+export LD_LIBRARY_PATH=${ROOT_PATH}/ibverbs_intercept:$LD_LIBRARY_PATH
+module load openmpi
+
+for size in 8 16 32 64 128 256 512 1024 2048; do
+    JOBTAG=size_${size} 
+    WORKLOAD=${ROOT_PATH}/astra-sim/examples/genie/workload/microbenchmark/30iter/4npus/${COLLECTIVE_UPPER}_${size} \
+    JOBTAG=${JOBTAG} \
+    bash ${RUN_SCRIPT}
+done
+
+mv genie*.json "${OUTPUT_PATH}/"
+mv output*.log "${OUTPUT_PATH}/"
